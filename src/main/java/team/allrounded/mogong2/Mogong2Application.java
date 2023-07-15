db0dafc1005c3ea782f1b993f357113d4978dc45
nathan29849 <phs960418@gmail.com>
2023-07-15 20:37:39 +0900

    init project

---
+++ src/main/java/team/allrounded/mogong2/Mogong2Application.java
@@ -0,0 +1,13 @@
+package team.allrounded.mogong2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mogong2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mogong2Application.class, args);
+	}
+
+}
---
+++ src/test/java/team/allrounded/mogong2/Mogong2ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.allrounded.mogong2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mogong2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
